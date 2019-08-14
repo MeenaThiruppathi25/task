9dfcbb3082508a92ab2322c14caf0a3a17f50310
Meena.T <meena.t@crystaldelta.com>
2019-08-14 15:07:14 +0530

    instagram integrated

---
+++ config/initializers/instagram.rb
@@ -0,0 +1,5 @@
+require "instagram"
+    Instagram.configure do |config|
+    config.client_id = "2e54e99487ee4bfe811729b47d85d551"
+    config.access_token = "5679969082.2e54e99.f8ba50eb6ec84a3eba103197fa20ad24"
+ end
